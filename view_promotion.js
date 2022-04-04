52f12efd77b990b07ed99974ff20d6ec20c0079a
Carlos M. Díaz Honrado <carlos@carlosmdh.es>
2022-04-04 13:55:00 +0200

    Create view_promotion.js

---
+++ view_promotion.js
@@ -0,0 +1,22 @@
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+  event: 'view_promotion',
+  ecommerce: {
+    items: [{
+      promotion_id: 'nvd21',                 // ID de la promoción
+      promotion_name: 'navidad_2021',       // Nombre de la campaña de promoción
+      creative_name: 'dto_50_por_ciento',   // Mensaje del banner de la promoción
+      creative_slot: 'sidebar1',            // Posición del banner (puede haber varios elementos en la misma ubicación, por ejemplo,en un slider)
+      location_id: 'sidebar'                // Ubicación del banner
+    },                                      // Fin primera promoción
+    {
+      promotion_id: 'nvd21',                // ID de la promoción
+      promotion_name: 'navidad_2021',       // Nombre de la campaña de promoción
+      creative_name: 'envios_gratis_compras_mayores de 100_euros',  // Mensaje del banner de la promoción
+      creative_slot: 'sidebar2',            // Posición del banner (puede haber varios elementos en la misma ubicación, por ejemplo,en un slider)
+      location_id: 'sidebar'                // Ubicación del banner
+    }                                       // Fin segunda promoción
+    ]                                       // Fin array items
+  }                                         // Fin objeto ecommerce
+}                                           // Fin datalayer.push
+);
